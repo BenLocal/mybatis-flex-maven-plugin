027ced245db61b96f47bd8183eb0d229c6b59919
benshi <807629978@qq.com>
2025-05-28 00:21:00 +0800

    add nullable to java.util.Optional<?>

--- src/main/java/com/github/benshi/mybatis/flex/plugin/GeneratorMojo.java
+++ src/main/java/com/github/benshi/mybatis/flex/plugin/GeneratorMojo.java
@@ -1,6 +1,7 @@
 package com.github.benshi.mybatis.flex.plugin;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -11,9 +12,11 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import com.mybatisflex.codegen.Generator;
+import com.mybatisflex.codegen.config.ColumnConfig;
 import com.mybatisflex.codegen.config.GlobalConfig;
 import com.mybatisflex.codegen.entity.Column;
 import com.mybatisflex.codegen.entity.Table;
+import com.mybatisflex.core.handler.OptionalTypeHandler;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
@@ -57,14 +60,46 @@ public class GeneratorMojo extends AbstractMojo {
                 if (JavaKeywordsUtils.isJavaKeyword(columnName) ||
                         JavaKeywordsUtils.startWithNumberKeyword(columnName)) {
                     // If the column name is a Java keyword or starts with a number,
-                    String fieldName = String.format("_%s", columnName);
-                    column.setProperty(fieldName);
+                    column.setProperty(renameProperty(columnName));
                     getLog().warn(String.format(
                             "Column '%s' in table '%s' is renamed to '%s' to avoid conflicts with Java keywords.",
                             columnName, table.getName(), column.getProperty()));
                 }
+
+                if (column.getNullable() != null && column.getNullable() == 1) {
+                    getLog().info(String.format(
+                            "Column '%s' in table '%s' is nullable, changing type to java.util.Optional<%s>.",
+                            columnName, table.getName(), column.getPropertyType()));
+
+                    if (column.getColumnConfig() == null) {
+                        ColumnConfig cc = new ColumnConfig()
+                                .setTypeHandler(OptionalTypeHandler.class);
+                        column.setColumnConfig(cc);
+                    } else {
+                        ColumnConfig cc = column.getColumnConfig();
+                        if (cc.getTypeHandler() == null) {
+                            cc.setTypeHandler(OptionalTypeHandler.class);
+                        }
+                    }
+
+                    // Change the property type to Optional<>
+                    column.setPropertyType("java.util.Optional<" + column.getPropertyType() + ">");
+                }
             }
         }
+    }
+
+    private String renameProperty(String property) {
+        if (property == null || property.isEmpty()) {
+            return property;
+        }
+
+        String tmp = property.trim();
+        if (Character.isLowerCase(property.charAt(0))) {
+            tmp = Character.toUpperCase(property.charAt(0)) + property.substring(1);
+        }
+
+        return String.format("valueOf%s", tmp);
     }
 
     private GlobalConfig createGlobalConfig() {
