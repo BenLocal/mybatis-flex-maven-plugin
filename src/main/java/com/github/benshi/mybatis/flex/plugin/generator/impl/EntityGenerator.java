5fa206336a2a6d2a3af972149ae14d7c58f4fc90
benshi <807629978@qq.com>
2025-06-17 00:00:04 +0800

    feat(EntityGenerator): enhance entity generation with optional fields and Javadoc
    * Added support for generating getter and setter methods with `Optional` return types.
    * Improved Javadoc comments for better documentation of generated entities.
    * Updated `pom.xml` to include `jsr305` dependency for annotations.
    
    Signed-off-by: benshi <807629978@qq.com>

--- src/main/java/com/github/benshi/mybatis/flex/plugin/generator/impl/EntityGenerator.java
+++ src/main/java/com/github/benshi/mybatis/flex/plugin/generator/impl/EntityGenerator.java
@@ -1,14 +1,23 @@
 package com.github.benshi.mybatis.flex.plugin.generator.impl;
 
+import java.io.Serializable;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.lang.model.element.Modifier;
 
 import com.github.benshi.mybatis.flex.plugin.generator.IPoetGenerator;
 import com.mybatisflex.codegen.config.EntityConfig;
 import com.mybatisflex.codegen.config.GlobalConfig;
 import com.mybatisflex.codegen.config.PackageConfig;
+import com.mybatisflex.codegen.entity.Column;
 import com.mybatisflex.codegen.entity.Table;
 import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
 
 public class EntityGenerator implements IPoetGenerator {
@@ -26,7 +35,7 @@ public class EntityGenerator implements IPoetGenerator {
     private void genEntity(Table table, GlobalConfig globalConfig) {
         JavaFile javaFile = createJavaFile(table, globalConfig);
         if (javaFile != null) {
-            System.out.printf("Entity class %s generated successfully.%n", javaFile.toString());
+            System.out.printf("Entity class generated successfully:\n%s\n", javaFile.toString());
         } else {
             System.err.printf("Failed to generate entity class for table %s.%n", table.getName());
         }
@@ -43,16 +52,92 @@ public class EntityGenerator implements IPoetGenerator {
         PackageConfig packageConfig = globalConfig.getPackageConfig();
         String entityName = table.getEntityJavaFileName();
 
-        TypeSpec entityTypeSpec = TypeSpec.classBuilder(entityName)
+        TypeSpec.Builder entityTypeSpecBuilder = TypeSpec.classBuilder(entityName)
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(AnnotationSpec.builder(
                         com.mybatisflex.annotation.Table.class)
                         .addMember("value", "$S", table.getName())
                         .build())
-                .build();
+                .addSuperinterface(Serializable.class);
+
+        entityTypeSpecBuilder.addJavadoc("Entity class for table: $L\n", table.getName());
+        entityTypeSpecBuilder.addJavadoc("\n");
+        entityTypeSpecBuilder.addJavadoc("<pre>\n");
+        entityTypeSpecBuilder.addJavadoc("$L\n", table.getComment());
+        entityTypeSpecBuilder.addJavadoc("</pre>\n");
+        entityTypeSpecBuilder.addField(
+                FieldSpec.builder(long.class, "serialVersionUID", Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+                        .initializer("$LL", 1)
+                        .build());
+
+        for (Column column : table.getColumns()) {
+            ClassName columnType = ClassName.bestGuess(column.getPropertyType());
+
+            FieldSpec.Builder field = FieldSpec.builder(columnType, column.getProperty(), Modifier.PRIVATE);
+
+            String methodName = firstCharToUpperCase(column.getProperty());
+            MethodSpec.Builder getterMethod = MethodSpec.methodBuilder("get" + methodName)
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(columnType)
+                    .addStatement("return $N", column.getProperty())
+                    .addJavadoc("Gets the value of the $L property.\n", column.getProperty())
+                    .addJavadoc("\n")
+                    .addJavadoc("<pre>\n")
+                    .addJavadoc("$L\n", column.getComment())
+                    .addJavadoc("</pre>\n")
+                    .addJavadoc("\n")
+                    .addJavadoc("@return the value of the property\n");
+
+            if (column.getNullable() != null && column.getNullable() == 1) {
+                getterMethod.addAnnotation(AnnotationSpec.builder(Nullable.class).build());
+            }
+
+            MethodSpec.Builder setterMethod = MethodSpec.methodBuilder("set" + methodName)
+                    .addModifiers(Modifier.PUBLIC)
+                    .addParameter(columnType, column.getProperty())
+                    .addStatement("this.$N = $N", column.getProperty(), column.getProperty())
+                    .addJavadoc("Sets the value of the $L property.\n", column.getProperty())
+                    .addJavadoc("\n")
+                    .addJavadoc("<pre>\n")
+                    .addJavadoc("$L\n", column.getComment())
+                    .addJavadoc("</pre>\n")
+                    .addJavadoc("\n")
+                    .addJavadoc("@param $N the new value for the property\n", column.getProperty());
+
+            ParameterizedTypeName optionalType = ParameterizedTypeName.get(
+                    ClassName.get(java.util.Optional.class),
+                    columnType);
+            MethodSpec.Builder optionalMethod = MethodSpec.methodBuilder("get" + methodName + "WithOptional")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(optionalType)
+                    .addStatement("return Optional.ofNullable($N)", column.getProperty())
+                    .addJavadoc("Gets the value of the $L property wrapped in an Optional.\n", column.getProperty())
+                    .addJavadoc("\n")
+                    .addJavadoc("<pre>\n")
+                    .addJavadoc("$L\n", column.getComment())
+                    .addJavadoc("</pre>\n")
+                    .addJavadoc("\n")
+                    .addJavadoc("@return an Optional containing the value of the property, or empty if null\n")
+                    .addAnnotation(
+                            AnnotationSpec.builder(
+                                    Nonnull.class)
+                                    .build());
+
+            entityTypeSpecBuilder.addField(field.build());
+            entityTypeSpecBuilder.addMethod(getterMethod.build());
+            entityTypeSpecBuilder.addMethod(setterMethod.build());
+            entityTypeSpecBuilder.addMethod(optionalMethod.build());
+        }
 
         return JavaFile.builder(
-                packageConfig.getBasePackage(), entityTypeSpec)
+                packageConfig.getBasePackage(), entityTypeSpecBuilder.build())
                 .build();
+    }
+
+    private String firstCharToUpperCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
 }
--- src/test/java/com/github/benshi/mybatis/flex/plugin/generator/impl/EntityGeneratorTest.java
+++ src/test/java/com/github/benshi/mybatis/flex/plugin/generator/impl/EntityGeneratorTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.mybatisflex.codegen.config.ColumnConfig;
 import com.mybatisflex.codegen.config.GlobalConfig;
 import com.mybatisflex.codegen.config.GlobalConfig.FileType;
 import com.mybatisflex.codegen.entity.Column;
@@ -16,19 +17,25 @@ public class EntityGeneratorTest {
     void testGenerate() {
         Table table = new Table();
         table.setName("test_table");
-        table.setComment("Test Table");
+        table.setComment("Test Tabl\n11111");
 
         List<Column> columns = new ArrayList<>();
         Column column1 = new Column();
+        column1.setColumnConfig(new ColumnConfig());
         column1.setName("id");
         column1.setComment("Primary Key");
         column1.setRawType("int");
         column1.setPrimaryKey(true);
+        column1.setAutoIncrement(true);
+        column1.setPropertyType("java.lang.Integer");
         columns.add(column1);
         Column column2 = new Column();
+        column2.setColumnConfig(new ColumnConfig());
         column2.setName("name");
         column2.setComment("Name Column");
         column2.setRawType("varchar");
+        column2.setPropertyType("java.lang.String");
+        column2.setNullable(1);
         columns.add(column2);
         table.setColumns(columns);
         table.setColumns(columns);
